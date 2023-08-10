5d4c06a6c5b6bbb59646a557a3301135388573a4
Congzhou Yi <2721710Y@student.gla.ac.uk>
2023-08-10 15:22:11 +0100

    init

--- lib/widgets/MessageInputField.dart
+++ lib/widgets/MessageInputField.dart
@@ -67,80 +67,70 @@ class _MessageInputFieldState extends State<MessageInputField>
     }
   }
 
-
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onVerticalDragUpdate: (details) {
-        if (details.delta.dy < 0) {
-          _handleFlyButtonPress();
-        } else if (details.delta.dy > 0) {
-          _handleFlyButtonPress(resetText: true);
-        }
-      },
-      child: Stack(
-        children: [
-          AdviceField(key: adviceFieldKey),
-          AnimatedBuilder(
-            animation: _animationController,
-            builder: (context, child) {
-              return Transform.translate(
-                offset: Offset(
-                    0,
-                    -_animation.value *
-                        MediaQuery.of(context).size.height *
-                        0.16),
-                child: child,
-              );
-            },
-            child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-              padding: EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                color: Color(0xFFf4f3f6),
-                border: Border.all(color: Colors.black87, width: 1.5),
-                borderRadius: BorderRadius.all(Radius.circular(20.0)),
-              ),
-              child: Material(
-                color: cardColor,
-                child: Padding(
-                  padding: const EdgeInsets.all(5.0),
-                  child: Row(
-                    children: [
-                      IconButton(
-                        icon: Icon(LineAwesomeIcons.fly),
-                        onPressed: _handleFlyButtonPress,
-                      ),
-                      SizedBox(width: 10),
-                      Expanded(
-                        child: TextField(
-                          focusNode: widget.focusNode,
-                          style: const TextStyle(color: Colors.black87),
-                          controller: widget.textEditingController,
-                          onSubmitted: widget.onSubmitted,
-                          decoration: const InputDecoration.collapsed(
-                            hintText: "Let's go!",
-                            hintStyle: TextStyle(
-                              color: Color.fromARGB(255, 97, 97, 97),
-                            ),
+    return Stack(
+      children: [
+        AdviceField(key: adviceFieldKey),
+        AnimatedBuilder(
+          animation: _animationController,
+          builder: (context, child) {
+            return Transform.translate(
+              offset: Offset(
+                  0,
+                  -_animation.value *
+                      MediaQuery.of(context).size.height *
+                      0.16),
+              child: child,
+            );
+          },
+          child: Container(
+            margin: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: Color(0xFFf4f3f6),
+              border: Border.all(color: Colors.black87, width: 1.5),
+              borderRadius: BorderRadius.all(Radius.circular(20.0)),
+            ),
+            child: Material(
+              color: cardColor,
+              child: Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Row(
+                  children: [
+                    IconButton(
+                      icon: Icon(LineAwesomeIcons.fly),
+                      onPressed: _handleFlyButtonPress,
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: TextField(
+                        focusNode: widget.focusNode,
+                        style: const TextStyle(color: Colors.black87),
+                        controller: widget.textEditingController,
+                        onSubmitted: widget.onSubmitted,
+                        decoration: const InputDecoration.collapsed(
+                          hintText: "Let's go!",
+                          hintStyle: TextStyle(
+                            color: Color.fromARGB(255, 97, 97, 97),
                           ),
                         ),
                       ),
-                      SendButton(
-                        focusNode: widget.focusNode,
-                        textEditingController: widget.textEditingController,
-                        scrollListToEND: widget.scrollListToEND,
-                        sendMessageFCT: widget.sendMessageFCT,
-                        chatProvider: widget.chatProvider,
-                      ),
-                    ],
-                  ),
+                    ),
+                    SendButton(
+                      focusNode: widget.focusNode,
+                      textEditingController: widget.textEditingController,
+                      scrollListToEND: widget.scrollListToEND,
+                      sendMessageFCT: widget.sendMessageFCT,
+                      chatProvider: widget.chatProvider,
+                    ),
+                  ],
                 ),
               ),
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 
